be4c69e022ade7a0cdc65d7a6473bd1b1dbf6ebc
Olecya <OlecyaMarkina@yandex.ru>
2023-04-09 18:21:02 +0300

    VM v0.1 corr CORS

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,5 +1,10 @@
 const JWT_KEY_SECRET = 'TOOOP-SICRET';
+const allowedCors = [
+  'https://olecyamesto.nomoredomains.work/sign-in',
+  'http://olecyamesto.nomoredomains.work',
+  'localhost:3000'
+];
 
 module.exports = {
-  JWT_KEY_SECRET,
+  JWT_KEY_SECRET, allowedCors
 };
